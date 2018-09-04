2180561a607f75b129b14a35e8954c6b19b1fbcb
br3ntor <br3ntor@gmail.com>
2018-09-04 14:51:26 -0700

    More commit!

---
+++ js/simonRefactor.js
@@ -0,0 +1,7 @@
+// Simon game refactor
+const frequencies = [165, 220, 277, 330, 100];
+const simonGame = {};
+
+simonGame.sayHi = function() {  
+  return console.log('Hello my name is simon.');
+};
